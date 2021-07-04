3a0ef35fd9f7f624ae66ed5df788bbd10eba2500
Fazi462 <86908255+Fazi462@users.noreply.github.com>
2021-07-04 13:03:04 +0500

    Add files via upload

---
+++ db.php
@@ -0,0 +1,16 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "UMER@umer123.com";
+    $db="dakkshesh_food_app";
+    
+    // Create connection
+    $con = new mysqli($servername, $username, $password, $db);
+
+    // if($con){
+    //     echo "connected";
+    // }else{
+    //     echo "not connected";
+    // }
+
+?>
